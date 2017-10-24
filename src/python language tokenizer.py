64ef3c176c86ed8f2551af57dbc8007e4df2931f
Lim Ju-Peng <limjupeng@gmail.com>
2017-10-25 04:21:52 +0800

    python tokenizer: tokenizes methods, strings and comments. comments tokenizer is not working as intended.

---
+++ src/python language tokenizer.py
@@ -0,0 +1,66 @@
+from nltk import *
+import json
+from bs4 import BeautifulSoup
+import re
+import random
+import string
+
+
+stop_words = corpus.stopwords.words("english")
+
+with open('Dirty Result.json', 'r') as f:
+    try:
+        dataset = json.load(f)
+
+    except Exception as e:
+        print(e)
+
+mydict = [x.lower() for x in corpus.words.words()]
+ps = stem.PorterStemmer()
+lmtzr = stem.wordnet.WordNetLemmatizer()
+
+methods = []
+strings = []
+comments = []
+variables = []
+mywords = []
+for post in dataset[:]:
+    for texts in post['posts']:
+        for sent in sent_tokenize(BeautifulSoup(texts, 'html.parser').get_text()):
+            for token in regexp_tokenize(sent, "(?:\\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*\)[^\(\)]*)*\)[^\(\)]*)*\)[^\(\)]*)*\))"):
+                methods.append(token.lower())
+            for token in regexp_tokenize(sent, "(?:\"[^\"\n]+\w[^\"\n]+\")|(?:'[^\"\n]+\w[^\"\n]+')|(?:\"{3}[^\"\n]+\w[^\"\n]+\"{3})|(?:'{3}[^'{3}]+'{3})"):
+                strings.append(token.lower())
+            for token in regexp_tokenize(sent, "(?:\\b)(?:#.+)"):
+                comments.append(token.lower())
+##            for token in regexp_tokenize(sent, "(#.+)"):
+##                variables.append(token.lower())
+textDist = FreqDist(mywords)
+
+for i in sorted(set(mywords)):
+    print(i)
+    print('========')
+
+print("\n Top 20 words")
+for i in textDist.most_common(20):
+    print(i)
+
+postagged = []
+
+##print("\npostagged:")
+for w in pos_tag(mywords):
+    postagged.append(w)
+##    print(w)
+
+##irregulars = []
+##for token in mywords:
+##    lemmaed_token = lmtzr.lemmatize(token.lower())
+##    stemmed_token = ps.stem(token.lower())
+##    lemmaedV_token = lmtzr.lemmatize(token.lower(), 'v')
+####    token = newtoken
+##    if token.lower() not in stop_words and lemmaed_token not in mydict and lemmaedV_token not in mydict and stemmed_token not in mydict:
+##        irregulars.append(token.lower())
+##irregularsDist = FreqDist(irregulars)
+
+
+
