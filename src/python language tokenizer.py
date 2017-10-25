b25cf5f579c9f5d182402d06be6034d0695396bf
JupsLaptop\Jups <limjupeng@gmail.com>
2017-10-25 17:22:22 +0800

    added POStagging of python lexicals, fixed string regex. comments regex still a problem

--- src/python language tokenizer.py
+++ src/python language tokenizer.py
@@ -5,6 +5,15 @@ import re
 import random
 import string
 
+methodpattern = [(r"(?:\\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*\)[^\(\)]*)*\)[^\(\)]*)*\)[^\(\)]*)*\))",'METHOD')]
+stringpattern = [(r"(?<!\w)(?:(?:\"{3}(?:.|\n)+?\"{3})|(?:'{3}(?:.|\n)+?'{3})|(?:\"[^\"\n]*\w[^\"\n]*\")|(?:'[^'\n]*\w[^'\n]*'))(?!\w)",'STRING')]
+commentpattern = [(r"(?:\\b)(?:#.+)",'COMMENT')]
+variablepattern = [(r'(?:\\b)((?:[a-zA-Z]|_)(?:(?:\.(?:_|\w)|(?:_|\w)))*\((?:.*)\))','VARIABLE')]
+
+methodtagger = RegexpTagger(methodpattern)
+stringtagger = RegexpTagger(stringpattern)
+commenttagger = RegexpTagger(commentpattern)
+variabletagger = RegexpTagger(variablepattern)
 
 stop_words = corpus.stopwords.words("english")
 
@@ -24,14 +33,14 @@ strings = []
 comments = []
 variables = []
 mywords = []
-for post in dataset[:5]:
+for post in dataset[5:10]:
     for texts in post['answers']:
         for sent in sent_tokenize(BeautifulSoup(texts, 'html.parser').get_text()):
-            for token in regexp_tokenize(sent, "(?:\\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*\)[^\(\)]*)*\)[^\(\)]*)*\)[^\(\)]*)*\))"):
-                methods.append(token.lower())
-            for token in regexp_tokenize(sent, "(?:\"[^\"\n]+\w[^\"\n]+\")|(?:'[^\"\n]+\w[^\"\n]+')|(?:\"{3}[^\"\n]+\w[^\"\n]+\"{3})|(?:'{3}[^'{3}]+'{3})"):
-                strings.append(token.lower())
-            for token in regexp_tokenize(sent, "(?:\\b)(?:#.+)"):
+##            for token in regexp_tokenize(sent, methodpattern[0][0]):
+##                methods.append(token.lower())
+##            for token in regexp_tokenize(sent, stringpattern[0][0]):
+##                strings.append(token.lower())
+            for token in regexp_tokenize(sent, "(?:\\b)(#.+[^\n])"):
                 comments.append(token.lower())
 ##            for token in regexp_tokenize(sent, "(#.+)"):
 ##                variables.append(token.lower())
@@ -47,9 +56,10 @@ for post in dataset[:5]:
 ##                variables.append(token.lower())
 textDist = FreqDist(mywords)
 
-for i in sorted(set(mywords)):
+for i in (set(comments)):
     print(i)
     print('========')
+
 
 print("\n Top 20 words")
 for i in textDist.most_common(20):
