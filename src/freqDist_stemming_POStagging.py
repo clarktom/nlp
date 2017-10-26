de654480e2768382d8608eaa4f1886d55d96ad59
JupsLaptop\Jups <limjupeng@gmail.com>
2017-10-26 17:41:57 +0800

    output text files for evaluation

--- src/freqDist_stemming_POStagging.py
+++ src/freqDist_stemming_POStagging.py
@@ -5,34 +5,33 @@ import re
 import random
 import string
 
-with open('../data/dirty_result.json', 'r') as f:
+with open('dirty_result.json', encoding = 'utf-8', mode='r') as f:
     try:
         dataset = json.load(f)
+        for i in range(len(dataset)):
+            dataset[i]['question'] = BeautifulSoup(dataset[i]['question'], 'html.parser').get_text()
+            for j in range(len(dataset[i]['answers'])):
+                dataset[i]['answers'][j] = BeautifulSoup(dataset[i]['answers'][j], 'html.parser').get_text()
 
     except Exception as e:
         print(e)
-
+contractions = ["n't", "'s", "s'", "'d", "'ll" , "'ve", "'re", "'m"] + [x for x in string.punctuation] + [ x for x in '0123456789']
 stop_words = corpus.stopwords.words("english")
-text = ""
 words = []
 sentences = []
-##for post in dataset[:10]:
-##    for texts in post['posts']:
-##        text += BeautifulSoup(texts, 'html.parser').get_text()
-##        text += ' '
-for post in dataset[:50]:
-    for texts in post['posts']:
-        for sent in sent_tokenize(BeautifulSoup(texts, 'html.parser').get_text()):
-            newstring = ''
-            for e in sent:
-                if e.isalpha():
-                    newstring += e.lower()
-                else:
-                    newstring += ' '
-            sentences.append(newstring)
-            for token in word_tokenize(newstring):
-                if token not in stop_words and token:
-                    words.append(token)
+
+for post in dataset:
+    for sent in sent_tokenize(post['question']):
+        sentences.append(sent)
+        for token in word_tokenize(sent):
+            if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
+                words.append(token.lower())
+    for texts in post['answers']:
+        for sent in sent_tokenize(texts):
+            sentences.append(sent)
+            for token in word_tokenize(sent):
+                if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
+                    words.append(token.lower())
 
 textDist = FreqDist(words)
 
@@ -50,26 +49,36 @@ for w in words:
 stemDist = FreqDist(stemmed)
 
 print("\n Top 20 stemmed")
-for i in stemDist.most_common(24):
+for i in stemDist.most_common(20):
     print(i)
 
 words_stemmed = []
-
-for stemmedword in stemDist.most_common(20):
-    print("")
-    for word in words:
-        if stemmedword[0] in word and word not in words_stemmed:
-            for i in range(len(stemmedword[0])):
-                if stemmedword[0][i] != word[i]:
-                    break
-                elif i == len(stemmedword[0][i])-1:
-                    words_stemmed.append(word)
-                    print(word)
-
+with open('top 20 stemmed.txt', encoding="utf-8",mode='w') as f:
+    for stemmedword in stemDist.most_common(20):
+        f.write('\n%s\n' %'#######################################')
+        f.write('stemmed word: %s\n' %str(stemmedword))
+        f.write('%s\n' %'showing all unique tokens')
+        f.write("\n")
+        for word in words:
+            if stemmedword[0] in word and word not in words_stemmed:
+                for i in range(len(stemmedword[0])):
+                    if stemmedword[0][i] != word[i]:
+                        break
+                    elif i == len(stemmedword[0][i])-1:
+                        words_stemmed.append(word)
+                        f.write("%s\n" % word)
+    f.write('%s\n' %'#######################################')
 postagged = []
 
-##random = random.random()*len(sentences)
-##for i in range(10):
-##    for w in pos_tag(word_tokenize(sentences[int(random)])):
-##        postagged.append(w)
-##        print(w)
+
+
+
+with open('random 10 POStags.txt', encoding="utf-8",mode='w') as f:
+    for i in range(10):
+        f.write('\n%s\n' %'#######################################')
+        r = random.random()*len(sentences)
+        if len(sentences[int(r)])<2:
+            r = random.random()*len(sentences)
+        for w in pos_tag(word_tokenize(sentences[int(r)])):
+            f.write("%s\n" % str(w))
+    f.write('%s\n' %'#######################################')
--- src/python language tokenizer.py
+++ src/python language tokenizer.py
@@ -4,17 +4,17 @@ from bs4 import BeautifulSoup
 import re
 import random
 import string
+import time
 
 methodpattern = [(r"(?:\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*\)[^\(\)]*)*\)[^\(\)]*)*\)[^\(\)]*)*\))",'METHOD')]
 stringpattern = [(r"(?:(?<!\w)|r)(?:(?:\"{3}.+?\"{3})|(?:'{3}.+?'{3})|(?:\"(?:(?!\\n)[^\"])+?\")|(?:'(?:(?!\\n)[^'])+?'))(?!\w)",'STRING')]
 commentpattern = [(r"(?:#[^#\n] *?(?!\\n|\n)[\S].+?)(?=\\n|\n)",'COMMENT')]
-variablepattern = [(r'(?:\b)((?:[a-zA-Z]|_)(?:(?:\.(?:_|\w)|(?:_|\w)))*\((?:.*)\))','VARIABLE')]
+##variablepattern = [(r'(?:\b)((?:[a-zA-Z]|_)(?:(?:\.(?:_|\w)|(?:_|\w)))*\((?:.*)\))','VARIABLE')]
 
+pythontagger = RegexpTagger(methodpattern+stringpattern+commentpattern)
 methodtagger = RegexpTagger(methodpattern)
 stringtagger = RegexpTagger(stringpattern)
 commenttagger = RegexpTagger(commentpattern)
-variabletagger = RegexpTagger(variablepattern)
-
 
 with open('dirty_result.json', encoding = 'utf-8', mode='r') as f:
     try:
@@ -28,7 +28,7 @@ with open('dirty_result.json', encoding = 'utf-8', mode='r') as f:
         print(e)
 
 stop_words = corpus.stopwords.words("english")
-contractions = ["n't", "'s,", "s'", "'d", "'ll" , "'ve", "'re", "'m"]
+contractions = ["n't", "'s", "s'", "'d", "'ll" , "'ve", "'re", "'m"] + [x for x in string.punctuation] + [ x for x in '0123456789']
 mydict = [x.lower() for x in corpus.words.words()]
 ps = stem.PorterStemmer()
 lmtzr = stem.wordnet.WordNetLemmatizer()
@@ -42,12 +42,12 @@ newdataset = dataset
 i = 0
 
 for i in range(len(dataset)):
-    print(i)
+##    print(i)
     sentences = sent_tokenize(dataset[i]['question'])
     for k in range(len(sentences)):
         tokens = regexp_tokenize(sentences[k], methodpattern[0][0])
         for l in range(len(tokens)):
-            methods.append(tokens[l].lower)
+            methods.append(tokens[l].lower())
             newdataset[i]['question'] = dataset[i]['question'].replace(tokens[l],'')
         tokens = regexp_tokenize(sentences[k], stringpattern[0][0])
         for l in range(len(tokens)):
@@ -73,45 +73,75 @@ for i in range(len(dataset)):
                 comments.append(re.sub('\s+?$', '', tokens[l].lower()))
                 newdataset[i]['answers'][j] = dataset[i]['question'][j].replace(tokens[l],'')
 
-
+sentences = []
 for post in newdataset:
     for sent in sent_tokenize(post['question']):
-        for token in word_tokenize(sent):
-            if token.lower() not in stop_words and token not in contractions and token not in string.punctuation and not re.match("^[\W]+$", token):
+        sentences.append(sent)
+        for token in word_tokenize(sent.lower()):
+            if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
                 mywords.append(token.lower())
     for texts in post['answers']:
         for sent in sent_tokenize(texts):
+            sentences.append(sent.lower())
             for token in word_tokenize(sent):
-                if token.lower() not in stop_words and token not in contractions and token not in string.punctuation and not re.match("^[\W]+$", token):
+                if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
                     mywords.append(token.lower())
-                    
+
 textDist = FreqDist(methods+strings+comments+mywords)
 
 ##for i in sorted(set(comments)):
 ##    print(i)
 ##    print('========')
 
-
 print("\n Top 20 words")
 for i in textDist.most_common(20):
     print(i)
 
-postagged = []
 
-##print("\npostagged:")
-for w in pos_tag(mywords):
-    postagged.append(w)
-##    print(w)
+irregularWords = []
 
-##irregulars = []
-##for token in mywords:
-##    lemmaed_token = lmtzr.lemmatize(token.lower())
-##    stemmed_token = ps.stem(token.lower())
-##    lemmaedV_token = lmtzr.lemmatize(token.lower(), 'v')
-####    token = newtoken
-##    if token.lower() not in stop_words and lemmaed_token not in mydict and lemmaedV_token not in mydict and stemmed_token not in mydict:
-##        irregulars.append(token.lower())
-##irregularsDist = FreqDist(irregulars)
+##print(len(mywords))
+start = time.time()
+for token in mywords:
+    lemmaed_token = lmtzr.lemmatize(token.lower())
+    stemmed_token = ps.stem(token.lower())
+    lemmaedV_token = lmtzr.lemmatize(token.lower(), 'v')
+##    print('next token')
+    if token.lower() not in stop_words and lemmaed_token not in mydict and lemmaedV_token not in mydict and stemmed_token not in mydict:
+        irregularWords.append(token.lower())
+print('time taken to check for irregulars: %f' %(time.time()-start)
 
+irregularWordsDist = FreqDist(irregularWords)
+print("\n Top 20 irregular words")
+top20irregulars = []
+with open('top 20 irregulars.txt', encoding="utf-8",mode='w') as f:
+    for irregularWord in irregularWordsDist.most_common(20):
+        print(irregularWord)
+        f.write('\n%s\n' %'#######################################')
+        f.write('irregular word: %s\n' %str(irregularWord))
+        f.write('%s\n' %'showing all unique tokens')
+        f.write("\n")
+        for word in mywords:
+            if irregularWord[0] in word and word not in top20irregulars:
+                for i in range(len(irregularWord[0])):
+                    if irregularWord[0][i] != word[i]:
+                        break
+                    elif i == len(irregularWord[0][i])-1:
+                        top20irregulars.append(word)
+                        f.write("%s\n" % word)
+    f.write('%s\n' %'#######################################')
 
-
+irregularWordSentences = []
+for sent in sentences:
+    for word in irregularWords:
+        if word in sent and sent not in irregularWordSentences:
+            irregularWordSentences.append(sent)
+with open('random 10 POStags with irregulars.txt', encoding="utf-8",mode='w') as f:
+    for i in range(10):
+        f.write('\n%s\n' %'#######################################')
+        r = random.random()*len(irregularWordSentences)
+        if len(irregularWordSentences[int(r)])<2:
+            r = random.random()*len(irregularWordSentences)
+        for w in pos_tag(word_tokenize(irregularWordSentences[int(r)])):
+            f.write("%s\n" % str(w))
+    f.write('%s\n' %'#######################################')
