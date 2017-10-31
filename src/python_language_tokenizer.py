0b15f0b41e467b1b22e2d0f178b5267de35cddd8
Lim Ju-Peng <limjupeng@gmail.com>
2017-11-01 02:43:30 +0800

    added URL regex, URL list, proper sentence tokenizing

--- src/python_language_tokenizer.py
+++ src/python_language_tokenizer.py
@@ -10,13 +10,15 @@ methodpattern = [(r"(?:\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:
 stringpattern = [(r"(?:(?<!\w)|r)(?:(?:\"{3}.+?\"{3})|(?:'{3}.+?'{3})|(?:\"(?:(?!\\n)[^\"])+?\")|(?:'(?:(?!\\n)[^'])+?'))(?!\w)",'STRING')]
 commentpattern = [(r"(?:#[^#\n] *?(?!\\n|\n)[\S].+?)(?=\\n|\n)",'COMMENT')]
 variablepattern = [(r'(?:(?<=\s)|^)(?:(?:[a-zA-Z_])(?:\.\w|\w)*?(?= *=)|(?:[a-zA-Z_])(?:\.\w|\w)*?(?=(?: *, *[a-zA-Z_][\w\.]*?)+(?= *=)))','VARIABLE')]
+URLpattern = [(r'(?:(?:https?:\/\/)?(?:[\da-z\.-]+)\.(?:[a-z\.]{2,6})(?:[\/\w \.-]*)*\/?)','URL')]
 
 POS_patterns = [(r"(?:\b)(?:[a-zA-Z_])(?:\.\w|\w)*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*(?:\([^\(\)]*\)[^\(\)]*)*\)[^\(\)]*)*\)[^\(\)]*)*\))",'METHOD'),
                 (r"(?:(?<!\w)|r)(?:(?:\"{3}.+?\"{3})|(?:'{3}.+?'{3})|(?:\"(?:(?!\\n)[^\"])+?\")|(?:'(?:(?!\\n)[^'])+?'))(?!\w)",'STRING'),
                 (r"(?:#[^#\n] *?(?!\\n|\n)[\S].+?)(?=\\n|\n)",'COMMENT'),
-                (r'(?:(?<=\s)|^)(?:(?:[a-zA-Z_])(?:\.\w|\w)*?(?= *=)|(?:[a-zA-Z_])(?:\.\w|\w)*?(?=(?: *, *[a-zA-Z_][\w\.]*?)+(?= *=)))','VARIABLE')]
+                (r'(?:(?<=\s)|^)(?:(?:[a-zA-Z_])(?:\.\w|\w)*?(?= *=)|(?:[a-zA-Z_])(?:\.\w|\w)*?(?=(?: *, *[a-zA-Z_][\w\.]*?)+(?= *=)))','VARIABLE'),
+                (r'(?:(?:https?:\/\/)?(?:[\da-z\.-]+)\.(?:[a-z\.]{2,6})(?:[\/\w \.-]*)*\/?)','URL')]
 
-pattern = POS_patterns[0][0]+'|'+POS_patterns[1][0]+'|'+POS_patterns[2][0]+'|'+POS_patterns[3][0]
+pattern = POS_patterns[0][0]+'|'+POS_patterns[1][0]+'|'+POS_patterns[2][0]+'|'+POS_patterns[3][0]+'|'+POS_patterns[4][0]
 
 start = time.time()
 with open('dirty_result.json', encoding = 'utf-8', mode='r') as f:
@@ -46,6 +48,9 @@ with open('dirty_result.json', encoding = 'utf-8', mode='r') as f:
                 dataset_IR[i]['answers'][j] = BeautifulSoup(dataset_IR[i]['answers'][j], 'html.parser').get_text()
     except Exception as e:
         print(e)
+
+
+        
 stop_words = set(corpus.stopwords.words("english"))
 contractions = set(["n't", "'s", "s'", "'d", "'ll" , "'ve", "'re", "'m"] + [x for x in string.punctuation] + [ x for x in '0123456789'])
 mydict = set([x.lower() for x in corpus.words.words()])
@@ -60,6 +65,7 @@ strings = []
 comments = []
 variables = []
 mywords = []
+URLs = []
 newdataset = dataset
 dataset_IR = dataset[:]
 i = 0
@@ -74,53 +80,61 @@ for i in range(len(dataset)):
         tokens = regexp_tokenize(sentences[k], methodpattern[0][0])
         for l in range(len(tokens)):
             methods.append(tokens[l].lower())
-            newdataset[i]['question'] = dataset[i]['question'].replace(tokens[l],'')
+            newdataset[i]['question'] = newdataset[i]['question'].replace(tokens[l],'')
         tokens = regexp_tokenize(sentences[k], stringpattern[0][0])
         for l in range(len(tokens)):
             strings.append(tokens[l].lower())
-            newdataset[i]['question'] = dataset[i]['question'].replace(tokens[l],'')
+            newdataset[i]['question'] = newdataset[i]['question'].replace(tokens[l],'')
         tokens = regexp_tokenize(sentences[k], commentpattern[0][0])
         for l in range(len(tokens)):
             comments.append(re.sub('\s+?$', '', tokens[l].lower()))
-            newdataset[i]['question'] = dataset[i]['question'].replace(tokens[l],'')
+            newdataset[i]['question'] = newdataset[i]['question'].replace(tokens[l],'')
         tokens = regexp_tokenize(sentences[k], variablepattern[0][0])
         for l in range(len(tokens)):
             variables.append(tokens[l].lower())
-            newdataset[i]['question'] = dataset[i]['question'].replace(tokens[l],'')
+##            newdataset[i]['question'] = newdataset[i]['question'].replace(tokens[l],'')
+        tokens = regexp_tokenize(sentences[k], URLpattern[0][0])
+        for l in range(len(tokens)):
+            URLs.append(tokens[l].lower())
+            newdataset[i]['question'] = newdataset[i]['question'].replace(tokens[l],'')
     for j in range(len(dataset[i]['answers'])):
         sentences = sent_tokenize(dataset[i]['answers'][j])
         for k in range(len(sentences)):
             tokens = regexp_tokenize(sentences[k], methodpattern[0][0])
             for l in range(len(tokens)):
                 methods.append(tokens[l].lower())
-                newdataset[i]['answers'][j] = dataset[i]['answers'][j].replace(tokens[l],'')
+                newdataset[i]['answers'][j] = newdataset[i]['answers'][j].replace(tokens[l],'')
             tokens = regexp_tokenize(sentences[k], stringpattern[0][0])
             for l in range(len(tokens)):
                 strings.append(tokens[l].lower())
-                newdataset[i]['answers'][j] = dataset[i]['answers'][j].replace(tokens[l],'')
+                newdataset[i]['answers'][j] = newdataset[i]['answers'][j].replace(tokens[l],'')
             tokens = regexp_tokenize(sentences[k], commentpattern[0][0])
             for l in range(len(tokens)):
                 comments.append(re.sub('\s+?$', '', tokens[l].lower()))
-                newdataset[i]['answers'][j] = dataset[i]['answers'][j].replace(tokens[l],'')
+                newdataset[i]['answers'][j] = newdataset[i]['answers'][j].replace(tokens[l],'')
             tokens = regexp_tokenize(sentences[k], variablepattern[0][0])
             for l in range(len(tokens)):
                 variables.append(tokens[l].lower())
-                newdataset[i]['answers'][j] = dataset[i]['answers'][j].replace(tokens[l],'')
+##                newdataset[i]['answers'][j] = newdataset[i]['answers'][j].replace(tokens[l],'')
+            tokens = regexp_tokenize(sentences[k], URLpattern[0][0])
+            for l in range(len(tokens)):
+                URLs.append(tokens[l].lower())
+                newdataset[i]['answers'][j] = newdataset[i]['answers'][j].replace(tokens[l],'')
 
 
 for post in newdataset:
     for sent in sent_tokenize(post['question']):
         for token in word_tokenize(sent.lower()):
-            if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
+            if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token) and token not in variables:
                 mywords.append(token.lower())
     for texts in post['answers']:
         for sent in sent_tokenize(texts):
             sentences.append(sent.lower())
             for token in word_tokenize(sent):
-                if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token):
+                if len(token) > 1 and token.lower() not in stop_words and token not in contractions and not re.search("^[\W\d]+$", token) and token not in variables:
                     mywords.append(token.lower())
 
-textDist = FreqDist(methods+strings+comments+mywords)
+textDist = FreqDist(methods+strings+comments+URLs+variables+mywords)
 print('time taken to tokenize: %f' %(time.time()-start))
 
 ##for i in sorted(set(comments)):
@@ -130,19 +144,6 @@ print('time taken to tokenize: %f' %(time.time()-start))
 ##print("\n Top 20 words")
 ##for i in textDist.most_common(20):
 ##    print(i)
-
-def binarySearch(word, array):
-    start, end = 0, (len(array) - 1)
-    while start <= end:
-        mid = (start + end) // 2
-        if word in array[mid]:
-            return (True,array[mid])
-        if word < array[mid]:
-            end = mid - 1
-        else:  # elem > arr[mid]
-            start = mid + 1
-
-    return (False,None)
 
 
 irregularWords = []
@@ -154,11 +155,6 @@ for token in mywords:
     if i % 10000 == 0:
         print(i)
     i-=1
-##    search = binarySearch(token, sorted(set(irregularWords)))
-##    if search[0]:
-####        print('yay')
-##        irregularWords.append(search[1])
-##        continue
     if token not in stop_words and token not in contractions:
         lemmaed_token = lmtzr.lemmatize(token)
         stemmed_token = ps.stem(token)
@@ -166,6 +162,8 @@ for token in mywords:
         if lemmaed_token not in mydict and lemmaedV_token not in mydict and stemmed_token not in mydict:
             irregularWords.append(token)
 print('time taken to check for irregulars: %f' %(time.time()-start))
+
+
 
 irregularWordsDist = FreqDist(irregularWords)
 ##print("\n Top 20 irregular words")
@@ -180,35 +178,72 @@ with open('top_20_irregulars.txt', encoding="utf-8",mode='w') as f:
         f.write("\n")
         for word in irregularWords:
             if irregularWord[0] in word and word not in set(top20irregulars):
-                if re.match(irregularWord[0],word):
+                if re.match(re.escape(irregularWord[0]),word):
                     top20irregulars.append(word)
                     f.write("%s\n" % word)
-##                for i in range(len(irregularWord[0])):
-##                    if irregularWord[0][i] != word[i]:
-##                        break
-##                    elif i == len(irregularWord[0][i])-1:
-##                        top20irregulars.append(word)
-##                        f.write("%s\n" % word)
     f.write('%s\n' %'#######################################')
 
 print('time taken to consolidate irregulars: %f' %(time.time()-start))
-##for i in range(len(dataset_IR)):
-##    dataset_IR[i]['question'] = dataset_IR[i]['question'].replace(r'\n', '. ')
-##    for j in range(len(dataset[i]['answers'])):
-##        dataset_IR[i]['answers'][j] = dataset_IR[i]['answers'][j].replace(r'\n', '. ')
+
+
+
 sentences = []
-for post in dataset_IR:
-    sentences_with_newline = [sent.split('\n') for sent in sent_tokenize(post['question'])]
-    for sentence in sentences_with_newline:
-        for sent in sentence:
+for i in range(len(dataset_IR)):
+    p=0
+    j=0
+    while j < len(dataset_IR[i]['question']):
+        if dataset_IR[i]['question'][j] == '\n':
+            if j == 0:
+                dataset_IR[i]['question'] = dataset_IR[i]['question'][1:]
+                j-=1
+            elif dataset_IR[i]['question'][j-1] == '\n':
+                dataset_IR[i]['question'] = dataset_IR[i]['question'][:j]+'.'+dataset_IR[i]['question'][j:]
+            elif dataset_IR[i]['question'][j-1] in contractions:
+                if not (re.search(pattern,dataset_IR[i]['question'][p:j])):
+                    if dataset_IR[i]['question'][j-1] in ':':
+                        dataset_IR[i]['question'] = dataset_IR[i]['question'][:j-1]+'.'+dataset_IR[i]['question'][j:]
+                    else:
+                        dataset_IR[i]['question'] = dataset_IR[i]['question'][:j]+' '+dataset_IR[i]['question'][j:]
+                    
+            p = j
+        j+=1
+
+    for k in range(len(dataset_IR[i]['answers'])):
+        p=0
+        j=0
+        while j < len(dataset_IR[i]['answers'][k]):
+            if dataset_IR[i]['answers'][k][j] == '\n':
+                if j == 0:
+                        dataset_IR[i]['answers'][k] = dataset_IR[i]['answers'][k][1:]
+                        j-=1
+                elif dataset_IR[i]['answers'][k][j-1] == '\n':
+                        dataset_IR[i]['answers'][k] = dataset_IR[i]['answers'][k][:j] + '.' + dataset_IR[i]['answers'][k][j:]
+                        j-=1
+                elif dataset_IR[i]['answers'][k][j-1] in contractions:
+                        if not (re.search(pattern,dataset_IR[i]['answers'][k][p:j])):
+                            if dataset_IR[i]['answers'][k][j-1] in ':':
+                                dataset_IR[i]['answers'][k] = dataset_IR[i]['answers'][k][:j-1] + '.' + dataset_IR[i]['answers'][k][j:]
+                            else:
+                                dataset_IR[i]['answers'][k] = dataset_IR[i]['answers'][k][:j] + ' ' + dataset_IR[i]['answers'][k][j:]
+                            
+                p = j
+            j+=1
+            
+
+    for sent in sent_tokenize(dataset_IR[i]['question']):
+        if re.search(pattern, sent):
+            sent=sent[:-1]
+        if len(sent)>2:
+            sentences.append(sent.lower())
+    for texts in dataset_IR[i]['answers']:
+        for sent in sent_tokenize(texts):
+            if re.search(pattern, sent):
+                sent=sent[:-1]
             if len(sent)>2:
                 sentences.append(sent.lower())
-    for texts in post['answers']:
-        sentences_with_newline = [sent.split('\n') for sent in sent_tokenize(texts)]
-        for sentence in sentences_with_newline:
-            for sent in sentence:
-                if len(sent)>2:
-                    sentences.append(sent.lower())
+
+
+
 start = time.time()
 print('there are '+str(len(set(sentences))*len(set(irregularWords)))+' comparisons to make')
 i=len(set(sentences))*len(set(irregularWords))
@@ -231,11 +266,12 @@ with open('random_10_POStags_with_irregulars.txt', encoding="utf-8",mode='w') as
         r = int(random.random()*len(irregularWordSentence))
         f.write('\n%s\n' %'#######################################')
         f.write("%s\n\n" % ('irregular word: '+str(irregularWordSentence[int(r)][0])))
-        tokens = regexp_tokenize(irregularWordSentence[int(r)][1], pattern)
+        tokens = set(regexp_tokenize(irregularWordSentence[int(r)][1], pattern))
         for l in range(len(tokens)):
-            irregularWordSentence[int(r)][1] = irregularWordSentence[int(r)][1].replace(tokens[l],'')
+            if len(tokens[l]>3):
+                irregularWordSentence[int(r)][1] = irregularWordSentence[int(r)][1].replace(tokens[l],'')
         for token in word_tokenize(irregularWordSentence[int(r)][1]):
-            tokens.append(token)
+            tokens.add(token)
         for w in pythontagger.tag(tokens):
             f.write("%s\n" % str(w))
     f.write('%s\n' %'#######################################')
